ae1d5ab887368289c0a8dc803bca2299e38129c4
trevorstam <trevorstam@gmail.com>
2018-08-26 22:36:31 -0700

    added postgres connection string

--- trevor/server.js
+++ trevor/server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
-const conString = '';
+const conString = 'postgres//trevorstam:1910@localhost:5432/kilovolt';
 const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => {
